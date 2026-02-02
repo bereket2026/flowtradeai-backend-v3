223871ba35d37085b14fc79094d70de65bd87bb7
bereket2026 <sebhatau@gmail.com>
2026-02-02 23:03:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FlowTradeAI backend is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("FlowTradeAI backend running on port", PORT);
+});
