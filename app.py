8c6bde13ab05eb9a2bcbbe9a172bfc1308f92798
bereket2026 <sebhatau@gmail.com>
2026-02-06 12:55:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,14 @@ import os
 app = Flask(__name__, static_folder="static")
 CORS(app)
 
-
-# ---------- HOME PAGE ----------
 @app.route("/")
 def home():
     return send_from_directory("static", "index.html")
 
-
-# ---------- TEST ACCOUNT API ----------
 @app.route("/account")
 def account():
-    return jsonify({
-        "balance": 1000,
-        "total_pnl": 25
-    })
+    return jsonify({"balance": 1000, "total_pnl": 25})
 
-
-# ---------- REQUIRED FOR RENDER ----------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
