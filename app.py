19379bb3655838a3be73d13599983b7bd773d528
bereket2026 <sebhatau@gmail.com>
2026-02-08 09:35:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
+import requests
 from flask import Flask, jsonify
-from binance.client import Client
 
 app = Flask(__name__)
-client = Client()
+
 
 @app.route("/")
 def home():
     return "FlowTradeAI Signal API is running 🚀"
 
+
 @app.route("/signal/<symbol>")
 def signal(symbol):
     try:
-        symbol = symbol.upper()   # IMPORTANT FIX
+        # convert BTCUSDT → btc
+        symbol_clean = symbol.lower().replace("usdt", "")
 
-        price = client.get_symbol_ticker(symbol=symbol)["price"]
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids={symbol_clean}&vs_currencies=usd"
+        response = requests.get(url, timeout=10)
+        data = response.json()
+
+        # check if symbol exists
+        if symbol_clean not in data:
+            return jsonify({"error": "Invalid symbol"}), 400
+
+        price = data[symbol_clean]["usd"]
 
         return jsonify({
-            "symbol": symbol,
+            "symbol": symbol.upper(),
             "price": price,
-            "signal": "BUY"  # simple test signal
+            "signal": "BUY"
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": "Server error", "details": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
